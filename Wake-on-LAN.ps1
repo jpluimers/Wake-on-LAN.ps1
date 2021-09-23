51df5e3ce509d6945cb74ef4786198ed26f53ae0
Jeroen Wiert Pluimers <jeroen.gitlab.com@pluimers.com>
2021-09-23 17:30:22 +0200

    Formatted to K&R/OTBS; alias expansion

--- Wake-on-LAN.ps1
+++ Wake-on-LAN.ps1
@@ -31,14 +31,14 @@ Make sure the MAC addresses supplied don't contain "-" or ".".
 #>
  
  
-param( [Parameter(Mandatory=$true, HelpMessage="MAC address of target machine to wake up")]
-       [string] $MacAddress )
- 
+param(
+    [Parameter(Mandatory = $true, HelpMessage = "MAC address of target machine to wake up")]
+    [string] $MacAddress
+) 
  
 Set-StrictMode -Version Latest
- 
-function Send-Packet([string]$MacAddress)
-{
+
+function Send-Packet([string]$MacAddress) {
     <#
     .SYNOPSIS
     Sends a number of magic packets using UDP broadcast.
@@ -50,8 +50,7 @@ function Send-Packet([string]$MacAddress)
     The MAC address of the machine to wake up.
     #>
  
-    try
-    {
+    try {
         $Broadcast = ([System.Net.IPAddress]::Broadcast)
  
         ## Create UDP client instance
@@ -64,19 +63,18 @@ function Send-Packet([string]$MacAddress)
         $MAC = [Net.NetworkInformation.PhysicalAddress]::Parse($MacAddress.ToUpper())
  
         ## Construct the Magic Packet frame
-        $Packet =  [Byte[]](,0xFF*6)+($MAC.GetAddressBytes()*16)
+        $Packet = [Byte[]](, 0xFF * 6) + ($MAC.GetAddressBytes() * 16)
  
         ## Broadcast UDP packets to the IP endpoint of the machine
         $UdpClient.Send($Packet, $Packet.Length, $IPEndPoint) | Out-Null
         $UdpClient.Close()
     }
-    catch
-    {
+    catch {
         $UdpClient.Dispose()
         $Error | Write-Error;
     }
 }
 
 ## Send magic packet to wake machine
-Write "Sending magic packet to $MacAddress"
+Write-Output "Sending magic packet to $MacAddress"
 Send-Packet $MacAddress
